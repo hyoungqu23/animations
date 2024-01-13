bc8bcf8ef1e9dd72b4cd465fa9a1041416fa4b42
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-01-13 22:59:34 +0900

    🎨 FullScreen UI를 추가한다

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
 import Image from 'next/image';
 
-export default function Home() {
+const Home = () => {
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='min-h-screen'>
+      <section className='min-h-screen bg-red-200 flex items-center justify-center text-5xl font-extrabold'>
+        1
+      </section>
+      <section className='min-h-screen bg-orange-200 flex items-center justify-center text-5xl font-extrabold'>
+        2
+      </section>
+      <section className='min-h-screen bg-yellow-200 flex items-center justify-center text-5xl font-extrabold'>
+        3
+      </section>
+      <section className='min-h-screen bg-green-200 flex items-center justify-center text-5xl font-extrabold'>
+        4
+      </section>
+      <section className='min-h-screen bg-blue-200 flex items-center justify-center text-5xl font-extrabold'>
+        5
+      </section>
+      <section className='min-h-screen bg-indigo-200 flex items-center justify-center text-5xl font-extrabold'>
+        6
+      </section>
+      <section className='min-h-screen bg-violet-200 flex items-center justify-center text-5xl font-extrabold'>
+        7
+      </section>
+    </main>
   );
-}
+};
+
+export default Home;
